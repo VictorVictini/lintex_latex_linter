afa283efc8b689560cacfe9a082052c0112c9042
Mohammad Danyal SE2022 <2804-pqb23142@users.noreply.gitlab.cis.strath.ac.uk>
2026-01-14 21:57:50 +0000

    retrieving form data

--- handlers.go
+++ handlers.go
@@ -4,16 +4,17 @@ import (
 	"html/template"
 	"net/http"
 	"regexp"
+	"strings"
 )
 
 type Page struct {
-	Title     string
-	Body      []byte
+	Body      string
 	Responses []string
 }
 
-var templates = template.Must(template.ParseFiles("webpages/home.html"))
-var validPath = regexp.MustCompile("^/home/?$")
+var funcs = template.FuncMap{"join": strings.Join}
+var templates = template.Must(template.New("").Funcs(funcs).ParseFiles("webpages/home.html"))
+var validPath = regexp.MustCompile("^/(home)/[a-zA-Z0-9/]*$")
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
@@ -29,13 +30,31 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 			http.NotFound(w, r)
 			return
 		}
-		fn(w, r, m[0])
+		fn(w, r, m[1])
 	}
 }
 
 func homepageHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p := &Page{
-		Title:     "LinTeX: Promoting Accessibility in LaTeX",
-		Responses: make([]string, 0)}
-	renderTemplate(w, "home", p)
+	// parse form
+	page := ProcessForm(r)
+
+	// retrieve error outputs
+
+	// output webpage
+	renderTemplate(w, "home", page)
+}
+
+func ProcessForm(r *http.Request) *Page {
+	// default for first load
+	defaultPage := &Page{}
+
+	// parse form data
+	err := r.ParseForm()
+	if err != nil {
+		return defaultPage
+	}
+
+	return &Page{
+		Body:      r.FormValue("body"),
+		Responses: strings.Split(r.FormValue("responses"), ",")}
 }
